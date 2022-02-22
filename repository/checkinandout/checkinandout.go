47cd458791403f95f8e65c1a088454594108047c
unknown <fidi.breaker@gmail.com>
2022-02-22 15:14:37 +0700

    added check in on repository

--- repository/checkinandout/checkinandout.go
+++ repository/checkinandout/checkinandout.go
@@ -2,6 +2,8 @@ package checkinandout
 
 import (
 	"database/sql"
+	"fmt"
+	"sirclo/entities"
 )
 
 type CheckRepository struct {
@@ -10,4 +12,16 @@ type CheckRepository struct {
 
 func New(db *sql.DB) *CheckRepository {
 	return &CheckRepository{db: db}
+}
+
+func (cr *CheckRepository) Checkin(attendance entities.CheckinAndOutResponseFormat) error {
+	result, err := cr.db.Exec("UPDATE attendances SET check_in = ?, check_temperature = ?, updated_at = now() WHERE id = ? AND deleted_at IS null", attendance.Checkin, attendance.CheckTemperature, attendance.User.Id)
+	if err != nil {
+		return err
+	}
+	mengubah, _ := result.RowsAffected()
+	if mengubah == 0 {
+		return fmt.Errorf("user not found")
+	}
+	return nil
 }
